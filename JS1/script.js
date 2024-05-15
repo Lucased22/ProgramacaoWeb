4b0a0874af22cc9afc8e4859bea8dae3cab20843
Lucased22 <lucaseduardo.siq@gmail.com>
2024-05-15 17:37:14 -0400

    creating JS1 exercise

---
+++ JS1/script.js
@@ -0,0 +1,22 @@
+// Função para criar uma tabela
+function createTable(tableIndex) {
+    // document.write("<h3>Tabela " + tableIndex + "</h3>");
+    document.write("<table>");
+    // Cabeçalho da tabela
+    document.write("<tr><th colspan='2'>Produtos de: " + tableIndex + " </th></tr>");
+    // Corpo da tabela
+    for (var i = 1; i <= 11; i++) {
+        document.write("<tr>");
+        document.write("<td>"+ tableIndex +"x" + (i - 1) + "</td>");
+        document.write("<td>" + (tableIndex*(i-1)) + "</td>");
+        document.write("</tr>");
+    }
+    document.write("</table>");
+}
+// document.write("<div>");
+// Criar as 10 tabelas
+for (var i = 1; i <= 10; i++) {
+    createTable(i);
+}
+
+// document.write("</div>");
