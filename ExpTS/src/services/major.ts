c46746fbb24faa5c9be852c17331d3890f93772f
Lucas Eduardo <lucaseduardo.siq@gmail.com>
2025-06-23 08:21:35 -0400

    adding expTs project

---
+++ ExpTS/src/services/major.ts
@@ -0,0 +1,16 @@
+import { PrismaClient, Major } from "../generated/prisma";
+import { CreateMajorDTO } from "../types/major";
+
+const prisma = new PrismaClient()
+
+export const getMajors = async (): Promise<Major[]> => {
+    return await prisma.major.findMany()
+}
+
+export const createMajor = async (major: CreateMajorDTO): Promise<Major> => {
+    return await prisma.major.create({ data: major })
+}
+
+export const getMajor = async (id: string): Promise<Major | null> => {
+    return await prisma.major.findFirst({ where: { id } })
+}
